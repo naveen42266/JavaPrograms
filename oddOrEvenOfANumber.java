f620ea6a1bf34e5d5756ab2c1a39d64bcebb76a2
Naveen <99822733+naveen42266@users.noreply.github.com>
2022-11-14 22:26:47 +0530

    Create oddOrEvenOfANumber.java

---
+++ oddOrEvenOfANumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main
+{
+	public static void main(String[] args) {
+	    Scanner in =new Scanner(System.in);
+	    int n=in.nextInt();
+      if(n%2==0){
+      		System.out.println("The given number is EVEN NUMBER");
+      }
+      else{
+          System.out.println("The given number is ODD NUMBER);
+      }
+	}
+}
