96b3335d775563f051875fec727964139d295e76
Naveen <99822733+naveen42266@users.noreply.github.com>
2022-12-13 21:38:27 +0530

    Create letterToAsciiNum.java

---
+++ letterToAsciiNum.java
@@ -0,0 +1,21 @@
+public class asciiNum {
+    public static void main(String[] args) {
+        char ch1='A';
+        char ch2='E';
+        char ch3='I';
+        char ch4='O';
+        char ch5='U';
+        char ch6=',';
+        int ascii1= ch1;
+        int ascii2=ch2;
+        int ascii3=ch3;
+        int ascii4=ch4;
+        int ascii5=ch5;
+        int ascii6=ch6;
+
+        System.out.println("The Ascii Value of "+ch1+" is "+ ascii1);
+        System.out.println("The Ascii Value of "+ch2+" is "+ ascii2);
+        System.out.println(ascii6);
+
+    }
+}
