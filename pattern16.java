26195ccf97656ca5e23da7b901c2f89251072549
Naveen <99822733+naveen42266@users.noreply.github.com>
2022-12-20 22:51:22 +0530

    Create pattern16.java

---
+++ pattern16.java
@@ -0,0 +1,36 @@
+// down star pattern
+import java.util.Scanner;
+
+public class pattern16 {
+
+public static void main(String[] args) {
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("Enter the number of rows: ");
+
+int rows = in.nextInt();
+
+for (int i = 0; i <= rows - 1; i++) { //For loop for Rows
+
+for (int j = 0; j <= i; j++) { //For loop for Column
+
+System.out.print(" "); // blank space
+
+}
+
+for (int k = 0; k <= rows - 1 - i; k++) { 
+
+System.out.print("*" + " "); // prints * and blank space
+
+}
+
+System.out.println(); //Next line
+
+}
+
+sc.close();
+
+}
+
+}
