c653762bb63b4fa70625d305b9c4ea46f908147d
Naveen <99822733+naveen42266@users.noreply.github.com>
2023-01-11 21:02:17 +0530

    Create pattern36.java

---
+++ pattern36.java
@@ -0,0 +1,27 @@
+/*
+5
+2 
+2 1 
+2 1 2 
+2 1 2 1 
+2 1 2 1 2 
+*/
+import java.util.Scanner;
+
+public class pattern36 {
+
+public static void main(String[] args) {
+
+Scanner in = new Scanner(System.in); 
+int rows = in.nextInt(); 
+for (int i = rows; i >= 1; i--) {
+     for (int j = rows; j >= i; j--) {
+         if(j%2==0)
+       System.out.print((1) + " "); 
+         else if(j%2!=0)
+       System.out.print((2)+" ");
+     }
+  System.out.println();
+}
+}
+}
