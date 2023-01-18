696cbed05ca9a4afea828d659867d252b6b08eba
Naveen <99822733+naveen42266@users.noreply.github.com>
2023-01-18 08:26:03 +0530

    Create pattern42.java

---
+++ pattern42.java
@@ -0,0 +1,31 @@
+/*
+2 
+2 3 
+3 4 4 
+4 5 5 6 
+6 7 7 8 8  
+*/
+import java.util.Scanner;
+
+public class pattern42 {
+
+public static void main(String[] args) {
+
+Scanner in = new Scanner(System.in); 
+int rows = in.nextInt(); 
+int a=2;
+for (int i = rows; i >= 1; i--) {
+     for (int j = rows; j >= i; j--) {
+         if(j%2==0){
+             a+=1;
+       System.out.print(a + " ");
+         }
+         else if(j%2!=0){
+             a+=0;
+       System.out.print(a+" ");
+         }
+     }
+  System.out.println();
+}
+}
+}
